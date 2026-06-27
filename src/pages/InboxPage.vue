09aa13008013260a9de178ee35f9243336041654
Fakhri17 <fakhricoffe@gmail.com>
2026-06-27 12:34:24 +0700

    Add scaffolding: CSS, Supabase client, and pages

---
+++ src/pages/InboxPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const username = route.params.username
+</script>
+
+<template>
+  <h1>Inbox: {{ username }}</h1>
+</template>
